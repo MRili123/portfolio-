357b1250827b289f15f90862e607be0bd173b698
MRili123 <iliasssjb2004@gmail.com>
2024-12-19 14:01:59 +0100

    ilias19

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import * as THREE from './libs/three/build/three.module.js';
-import { GLTFLoader } from './libs/three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.155.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 
 const container = document.querySelector('#model-container');
